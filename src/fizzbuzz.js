f96a5bc0c1a1d34a8d7a9111ec527009df081f08
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-26 19:52:34 -0800

    Se añade la funcionalidad de regresar el mismo numero para un numero que no siga las reglas del FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,5 @@
 function FizzBuzz(numero) {
-    return 1 + "";
+    return numero + "";
 }
 
 export default FizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,7 +1,10 @@
 import FizzBuzz from "./fizzbuzz.js";
 
 describe("Sumar", () => {
-    it("Se retorna el mismo numero para uno que no sigue las reglas", () => {
+    it("Se retorna el mismo numero para 1 que no sigue las reglas", () => {
         expect(FizzBuzz(1)).toEqual("1");
+    });
+    it("Se retorna el mismo numero para un numero que no siga las reglas del FizzBuzz", () => {
+        expect(FizzBuzz(2)).toEqual("2");
     });
 });
